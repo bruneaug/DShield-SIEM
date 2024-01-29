6bb8370d659a3bf93f00d491fd127f3c1d88c1a8
bruneaug <48228401+bruneaug@users.noreply.github.com>
2024-01-29 18:26:55 -0500

    Add files via upload

---
+++ sensor_scripts/ttylog.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+# Guy Bruneau, guybruneau@outlook.com
+# Date: 11 Jan 2024
+# Version: 0.5
+
+# This script is used to parse the TTY logs captured by the DShield Sensor.
+
+# List the files by day save in the /srv/cowrie/var/lib/cowrie/tty directory.
+
+TODAY=$(date "+%b %d")
+#TODAY=$(date "+%Y-%m-%d")
+YESTERDAY=$(date -d "1 hour ago" '+%Y-%m-%d')
+DIRECTORY="$HOME/ttylog"
+TTYDIR="/srv/cowrie/var/lib/cowrie/tty/"
+
+# Check if directory exist and create it if doesn't
+if [ ! -d "$DIRECTORY" ]; then
+   mkdir -p "$DIRECTORY"
+fi
+
+# Get a copy of playlog localy
+
+playlog="/srv/cowrie/bin/playlog"
+
+if [ ! -f "playlog" ]; then
+
+  # Make a copy of the playlog script to the script
+  # directory
+  /usr/bin/cp /srv/cowrie/bin/playlog ~/scripts
+
+  # rename python to python3
+  sed -i 's/python/python3/g' playlog
+
+fi
+
+#FILES=$(/usr/bin/ls -l /srv/cowrie/var/lib/cowrie/tty | awk '{ print $6" "$7" "$9 }' | grep "$TODAY")
+#array=`/usr/bin/ls -l /srv/cowrie/var/lib/cowrie/tty | grep "$TODAY" | awk '{ print "/home/guy/scripts/playlog /srv/cowrie/var/lib/cowrie/tty/"$9" > ../ttylog/"$9 }'`
+array=`/usr/bin/ls -l /srv/cowrie/var/lib/cowrie/tty | grep "$TODAY" | awk '{ print "~/scripts/playlog -b /srv/cowrie/var/lib/cowrie/tty/"$9" | txt2html >&1 --outfile ../ttylog/"$9".html" }'`
+filelist=`/usr/bin/ls -l /srv/cowrie/var/lib/cowrie/tty | grep "$TODAY" | awk '{ print $9 }'`
+
+   echo "${array[@]}" > test.txt
+   chmod 755 test.txt
+   exec ./test.txt
+
