80e619148c79437a8598ed86a46ee4f40500fe4e
bruneaug <48228401+bruneaug@users.noreply.github.com>
2024-02-18 18:25:05 -0500

    Add files via upload

--- AddOnScripts/startelk.sh
+++ AddOnScripts/startelk.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/usr/bin/bash
 
 # Guy Bruneau
 # Date: 4 Jan 2023
@@ -10,9 +10,11 @@
 
 # Used to activate Elasticsearch
 
-# Using default password to sudo commands: student 
+# Using default password to sudo commands: training
+# You need to change it to the account password in use.
 
 PASSWORD="training"
+ELK="/home/guy/DShield-SIEM"
 
 export HISTIGNORE='*sudo -S*'
 echo "Starting the ELK components..."
@@ -20,7 +22,7 @@ echo "Starting the ELK components..."
 echo "Increasing virtual memory to 262144..."
 echo $PASSWORD | sudo -S -k sysctl -w vm.max_map_count=262144
 
-cd ~/Dshield-ELK
+cd $ELK
 echo $PASSWORD | sudo -S -k docker compose up -d
 
 echo "Login http://IP:5601 with user: elastic and password: student"
