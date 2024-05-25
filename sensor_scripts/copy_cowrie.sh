243c020017717d89a4877d2b273c2d2f0bac2376
bruneaug <48228401+bruneaug@users.noreply.github.com>
2024-05-25 17:56:04 -0400

    Add files via upload

--- sensor_scripts/copy_cowrie.sh
+++ sensor_scripts/copy_cowrie.sh
@@ -4,17 +4,18 @@
 # Date: 12 Mar 2024
 # Version: 1.0
 
-# This script is used to keep a copy of the iptables.log daily logs.
+# This script is used to keep a copy of the cowrie and webhoneypot daily logs.
 
 # root cronjob running once a day
-# Copy the daily iptables logs at the end of the day
-# 58 23 * * * /home/guy/scripts/copy_iptables.sh > /dev/null 2>1&
-
+# Copy the daily cowrie & webhoneypot logs at the end of the day
+# 0 2 * * * /home/guy/scripts/copy_cowrie.sh > /dev/null 2>1&
 
 TODAY=$(date "+%Y-%m-%d")
 YESTERDAY=`date -d "1 day ago" '+%Y-%m-%d'`
 
 DIRECTORY="/srv/NSM/cowrie"
+DIRECTORY1="/srv/NSM/webhoneypot"
+
 
 # Check if directory exist and create it if doesn't
 if [ ! -d "$DIRECTORY" ]; then
@@ -25,5 +26,15 @@ else
         echo "Directory '$DIRECTORY' already exist."
 fi
 
-
 /usr/bin/cp /srv/cowrie/var/log/cowrie/cowrie.json.$YESTERDAY $DIRECTORY
+
+# Check if directory exist and create it if doesn't
+if [ ! -d "$DIRECTORY1" ]; then
+   mkdir -p "$DIRECTORY1"
+   echo "Directory '$DIRECTORY1' created."
+
+else
+        echo "Directory '$DIRECTORY1' already exist."
+fi
+
+/usr/bin/cp /srv/db/webhoneypot-$YESTERDAY.json $DIRECTORY1
