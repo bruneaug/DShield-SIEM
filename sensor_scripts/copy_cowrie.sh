9f42fd650f274ced522e69aad3e6af5a04ca9f7c
bruneaug <48228401+bruneaug@users.noreply.github.com>
2024-05-25 18:05:41 -0400

    Add files via upload

--- sensor_scripts/copy_cowrie.sh
+++ sensor_scripts/copy_cowrie.sh
@@ -4,7 +4,7 @@
 # Date: 12 Mar 2024
 # Version: 1.0
 
-# This script is used to keep a copy of the cowrie and webhoneypot daily logs.
+# This script is used to keep a copy of the cowrie, download and webhoneypot daily logs.
 
 # root cronjob running once a day
 # Copy the daily cowrie & webhoneypot logs at the end of the day
@@ -15,9 +15,11 @@ YESTERDAY=`date -d "1 day ago" '+%Y-%m-%d'`
 
 DIRECTORY="/srv/NSM/cowrie"
 DIRECTORY1="/srv/NSM/webhoneypot"
+DIRECTORY2="/srv/NSM/downloads"
 
 
 # Check if directory exist and create it if doesn't
+
 if [ ! -d "$DIRECTORY" ]; then
    mkdir -p "$DIRECTORY"
    echo "Directory '$DIRECTORY' created."
@@ -29,6 +31,7 @@ fi
 /usr/bin/cp /srv/cowrie/var/log/cowrie/cowrie.json.$YESTERDAY $DIRECTORY
 
 # Check if directory exist and create it if doesn't
+
 if [ ! -d "$DIRECTORY1" ]; then
    mkdir -p "$DIRECTORY1"
    echo "Directory '$DIRECTORY1' created."
@@ -38,3 +41,15 @@ else
 fi
 
 /usr/bin/cp /srv/db/webhoneypot-$YESTERDAY.json $DIRECTORY1
+
+# Check if directory exist and create it if doesn't
+
+if [ ! -d "$DIRECTORY2" ]; then
+   mkdir -p "$DIRECTORY2"
+   echo "Directory '$DIRECTORY2' created."
+
+else
+        echo "Directory '$DIRECTORY2' already exist."
+fi
+
+/usr/bin/cp /srv/cowrie/var/lib/cowrie/downloads/* $DIRECTORY2
