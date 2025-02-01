191ab9e94b76192cac06901685bf81e081957139
bruneaug <48228401+bruneaug@users.noreply.github.com>
2025-02-01 16:43:06 -0500

    Add files via upload

--- AddOnScripts/change_perms.sh
+++ AddOnScripts/change_perms.sh
@@ -3,16 +3,33 @@
 # Date: 31 Jan 2025
 # Version: 0.1
 
-#### This script runs on ELK server
+#### This script runs on ELK server to setup various permissions
+#### directory for ISC threat intel.
+
+DIRECTORY='/opt/intel'
 
 # Change permissions to these scripts to executable
 
 chmod 755 ~/DShield-SIEM/scripts/cowrie-setup.sh
 sudo chown root:root ~/DShield-SIEM/filebeat/filebeat.yml
+sudo chown root:root ~/DShield-SIEM/filebeat01/filebeat.yml
 sudo chmod 644 ~/DShield-SIEM/filebeat/filebeat.yml
 sudo chown -R root:root ~/DShield-SIEM/metricbeat/*
 sudo chmod 644 ~/DShield-SIEM/metricbeat/metricbeat.yml
 sudo chmod -R 644 ~/DShield-SIEM/metricbeat/modules.d/*
+
+# Create /opt/intel directory with local account for local user
+
+# Check if directory exist and create it if doesn't
+
+if [ ! -d "$DIRECTORY" ]; then
+   sudo mkdir -p "$DIRECTORY"
+   sudo chown $USER:$USER /opt/intel
+   echo "Directory '$DIRECTORY' created and permissions changed to '$USER'."
+
+else
+        echo "Directory '$DIRECTORY' already exist."
+fi
 
 # Check if the file exist and if it does, change the IP to ELK Server 
 
