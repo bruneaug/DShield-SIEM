63f56a42f4aab09b81a1da8a0420dcb8dc20958e
bruneaug <48228401+bruneaug@users.noreply.github.com>
2026-04-01 18:08:44 -0400

    Update sensor_files.sh

--- AddOnScripts/sensor_files.sh
+++ AddOnScripts/sensor_files.sh
@@ -41,12 +41,12 @@ scp -P 12222 ubuntu@remoteIP:/var/log/dshield.log  /home/guy/dshield/
 # Download webhoneypot logs
 # Make a copy of the file to local directory & change the permissions
 # Change /home/ubuntu & ubuntu:ubuntu to your local DShield sensor directory
-ssh -p 12222 ubuntu@emoteIP "sudo cp /srv/log/webhoneypot-$YESTERDAY.json /home/ubuntu"
-ssh -p 12222 ubuntu@emoteIP "sudo chown ubuntu:ubuntu /home/ubuntuwebhoneypot-$YESTERDAY.json"
+ssh -p 12222 ubuntu@emoteIP "sudo cp /srv/log/webhoneypot_$YESTERDAY.json /home/ubuntu"
+ssh -p 12222 ubuntu@emoteIP "sudo chown ubuntu:ubuntu /home/ubuntuwebhoneypot_$YESTERDAY.json"
 
-scp -P 12222 ubuntu@emoteIP:/home/ubuntu/webhoneypot-$YESTERDAY.json /home/guy/webhoneypot/
+scp -P 12222 ubuntu@emoteIP:/home/ubuntu/webhoneypot_$YESTERDAY.json /home/guy/webhoneypot/
 # Delete the transferred file form the DShield sensor home user directory
-ssh -p 12222 ubuntu@emoteIP "sudo rm -f /home/ubuntu/webhoneypot-$YESTERDAY.json"
+ssh -p 12222 ubuntu@emoteIP "sudo rm -f /home/ubuntu/webhoneypot_$YESTERDAY.json"
 
 # Download cowerie logs
 scp -P 12222 ubuntu@remoteIP:/srv/cowrie/var/log/cowrie/cowrie.json.$YESTERDAY  /home/guy/cowrie/
