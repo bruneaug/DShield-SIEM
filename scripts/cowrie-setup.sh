15ea0109f81c7d25a2f24db3bc3a0f7862302e30
bruneaug <48228401+bruneaug@users.noreply.github.com>
2026-08-05 19:13:43 -0400

    Update cowrie-setup.sh
    Fixed template for cowrie.ttylog loading

--- scripts/cowrie-setup.sh
+++ scripts/cowrie-setup.sh
@@ -58,8 +58,8 @@ $curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm
 $curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.webhoneypot --data-binary @cowrie-webhoneypot-index.json; echo
 
 echo "Setting up cowrie.ttylog"
-$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/cowrie.webhoneypot --data-binary @cowrie.ttylog-policy.json; echo
-$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.webhoneypot --data-binary @cowrie.ttylog-index.json; echo
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/cowrie.ttylog --data-binary @cowrie.ttylog-policy.json; echo
+$curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_index_template/cowrie.ttylog --data-binary @cowrie.ttylog-index.json; echo
 
 echo "Setting up ti.iscintel"
 $curlcmd -s -H 'Content-Type: application/x-ndjson' -XPUT https://es01:9200/_ilm/policy/ti.iscintel --data-binary @ti.iscintel-policy.json; echo
